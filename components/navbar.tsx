6c8a64e5faa927d527078ec170e185b7107f1d75
Leon Carbonell <leoncarbonell@gmail.com>
2024-10-18 09:44:55 +1100

    feat: added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import { HEADER_HEIGHT } from '@/lib/consts'
+
+function Navbar() {
+  return (
+    <div className="content-evenly px-10" style={{ height: HEADER_HEIGHT }}>
+      Home
+    </div>
+  )
+}
+
+export default Navbar
---
+++ lib/consts.ts
@@ -0,0 +1,3 @@
+export const HEADER_HEIGHT = '3rem'
+
+export const bodyHeightStyle = `calc(100vh - ${HEADER_HEIGHT})`
