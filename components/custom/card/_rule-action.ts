e1c4c30a13e80b78d1f6f348e8bc26246b8c5a34
Leon Carbonell <leoncarbonell@gmail.com>
2024-10-18 12:13:44 +1100

    added mutation call

---
+++ components/custom/card/_rule-action.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function upload_file() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+}
--- components/custom/card/rule.tsx
+++ components/custom/card/rule.tsx
@@ -1,9 +1,61 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { upload_file } from './_rule-action'
+
 function CardRule() {
+  const [title, setTitle] = useState('')
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: upload_file,
+  })
+
+  function submit() {
+    mutate()
+  }
+
   return (
-    <div className="min-h-32 w-full rounded-md border-2 border-solid border-orange-500 bg-orange-300">
-      dogs
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="min-h-32 w-full rounded-md border-2 border-solid border-orange-500 bg-orange-300 p-3">
+            {title}
+          </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>
+              <div className="grid grid-flow-row gap-4">
+                <Input
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="title"
+                />
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Input id="picture" type="file" />
+                </div>
+                <Button disabled={isPending} onClick={() => submit()}>
+                  Upload
+                </Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">Meow</div>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
